c7e2ebbbb037b7a2e6ccd6e3492d128958686b7a
iamOuko <oukoezzy@gmail.com>
2020-08-02 21:32:37 +0300

    create repo

--- js/scripts.js
+++ js/scripts.js
@@ -20,22 +20,35 @@ $(document).ready(function () {
         var crust = $('#crust').val();
         var crustName = $("#crust option:selected").text();
         var size = $('#size').val();
-        // var topping = $("input[type=checkbox]:checked").val()
+        var sizeName = $("#size option:selected").text();
+
         var topping = [];
+        var toppingName = []
         $.each($("input[type=checkbox]:checked"), function () {
             topping.push($(this).val());
+            toppingName.push($(this).val());
         });
         console.log(topping)
         var toppingsValue = 0;
         for (var i = 0; i < topping.length; i++) {
             toppingsValue += parseInt(topping[i]);
         }
-        console.log(toppingsValue)
+
 
         var tot = (parseInt(size) + parseInt(crust) + parseInt(toppingsValue));
         alert("Total cost is " + tot);
 
-        $("#myselect option:selected").text();
+        var toppingName = ['Pepperoni', 'Pineaple', 'Bacon', 'Cheese', 'Mushrooms'];
+        var toppingFinal = toppingName.join(', ')
+
+        var finalTotal = (parseInt(size) + parseInt(crust) + parseInt(toppingsValue));
+        $("#summary").append("<p>" + ' size:' + sizeName + ' crust:' + crustName + ' total:' + finalTotal + "</p>");
+
+        var overallPrice = sessionStorage.getItem("overallPrice")
+        var total = parseInt(finalTotal) + parseInt(overallPrice)
+        sessionStorage.setItem("overallPrice", total);
+        console.log(total)
+
     });
 
 
@@ -44,5 +57,9 @@ $(document).ready(function () {
         var location = prompt("enter ur location");
         alert("your order will be delivered to" + " " + location);
     });
+
+    $('#finalOrder').click(function () {
+        alert(total)
+    })
 })
 
